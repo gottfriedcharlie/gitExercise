ffd2e4ce8132c17affe8f6d35a1406e4c784cd41
gottfriedcharlie <93043695+gottfriedcharlie@users.noreply.github.com>
2025-10-15 20:53:06 -0400

    Initial Commit

---
+++ GitHubExercose/GitHubExercoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExercoseApp.swift
+//  GitHubExercose
+//
+//  Created by Charlie Gottfried on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExercoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubExercoseTests/GitHubExercoseTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubExercoseTests.swift
+//  GitHubExercoseTests
+//
+//  Created by Charlie Gottfried on 10/15/25.
+//
+
+import Testing
+
+struct GitHubExercoseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
